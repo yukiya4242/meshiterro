bb4cb3e361a15df7df9f9fcaf4b1de51ba70da6a
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-03-10 01:21:14 +0000

    コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:"homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
